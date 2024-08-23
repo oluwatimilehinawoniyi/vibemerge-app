76786d4371c281cb21d47bad7685be285141c331
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2024-08-23 08:43:32 +0100

    RESTART PROJECT WITH A DIFFERENT STACK

--- app/api/playlists/route.ts
+++
@@ -1,8 +0,0 @@
-import { getPlaylists } from "../../lib/spotify";
-
-export async function GET(request: Request) {
-  const response = await getPlaylists();
-  const data = await response.json();
-
-  return Response.json({ data });
-}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Hero from "./components/layout/Hero";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <Hero />
-    </main>
-  );
-}
